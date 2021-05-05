e7e6d7304e14e0afd9374dcce7a5bb31482d53dc
sjxer723 <sjtu_xiaosong@sjtu.edu.cn>
2021-05-05 11:25:44 +0800

    a version that works...

---
+++ E1332/single_period_cpu_with_IO/mux2x32.v
@@ -0,0 +1,13 @@
+//2-way 32 bits multiplexers
+
+module mux2x32 (
+    input [31: 0] a0,
+    input [31: 0] a1,
+    input sel,
+    output wire [31: 0] y
+);
+    
+   
+    assign y = (sel ? a1 : a0);
+   
+endmodule
